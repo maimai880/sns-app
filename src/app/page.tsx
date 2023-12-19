b7178a867e1dc91a5cb342491de0c14df4fd1b54
ao-takeda <ao_21n1103123@nnn.ed.jp>
2023-12-19 10:43:43 +0900

    認証機能の基礎を実装

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,14 @@
-export default function Home() {
+import {getServerSession} from "next-auth"
+import {nextAuthOptions} from "@/lib/next-auth"
+
+export default async function Home() {
+  const user = await getServerSession(nextAuthOptions)
+
   return (
-    <>sns-app</>
+    <main>
+      <h1>sns-app</h1>
+
+      <p>{JSON.stringify(user)}</p>
+    </main>
   )
 }
