d9e50eff3bda3ea565c918a1aed3ca223b266f94
ao-takeda <ao_21n1103123@nnn.ed.jp>
2023-12-25 21:46:58 +0900

    jwtを使わなくなったことでdbから取得する必要がなくなったので修正

--- src/lib/next-auth.ts
+++ src/lib/next-auth.ts
@@ -2,6 +2,7 @@ import type {NextAuthOptions} from "next-auth"
 import GoogleProvider from "next-auth/providers/google"
 import {PrismaAdapter} from "@next-auth/prisma-adapter"
 import prisma from "@/lib/prisma"
+import {User} from "@/prisma-types"
 
 export const nextAuthOptions: NextAuthOptions = {
   secret: process.env.NEXTAUTH_SECRET,
@@ -13,11 +14,9 @@ export const nextAuthOptions: NextAuthOptions = {
   ],
   adapter: PrismaAdapter(prisma),
   callbacks: {
-    // @ts-ignore
-    async session({session, }) {
-      const user = await prisma.user.findUnique({
-        where: {email: session.user?.email || ""}
-      })
+    session: async ({session, user: adapterUser}) => {
+      const user = adapterUser as User
+
       return {
         ...session,
         user: !user ? session.user : {
