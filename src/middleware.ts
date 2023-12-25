c96c24b3fb8addc2c313707dbc9b9f413e9eb50e
ao-takeda <ao_21n1103123@nnn.ed.jp>
2023-12-25 21:47:30 +0900

    プロフィール初期化中は他のページを表示できない様middlewareを導入

---
+++ src/middleware.ts
@@ -0,0 +1,15 @@
+import {NextRequest, NextResponse} from "next/server"
+
+export const middleware = async (req: NextRequest) => {
+  // NOTE: NextAuth(ver.5)でmiddlewareでもgetServerSessionが使えるようになるまでの応急処置
+  const res = await fetch(`${req.nextUrl.origin}/api/auth/session`, {
+    method: "GET",
+    headers: req.headers,
+  })
+  const session = await res.json()
+  if (!session) return
+
+  if (session.user.finishInit && !req.nextUrl.pathname.startsWith("/auth/init")) {
+    return NextResponse.rewrite(new URL("/auth/init", req.url))
+  }
+}
